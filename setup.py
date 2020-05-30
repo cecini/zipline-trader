76d410f8cd3c06c8cf06ba2885525f7ee145551e
Shlomi Kushchi <shlomik@gmail.com>
2020-05-30 12:35:13 +0300

    rename project

--- setup.py
+++ setup.py
@@ -263,8 +263,8 @@ if 'sdist' in sys.argv:
 
 
 setup(
-    name='zipline-livetrader',
-    url="https://github.com/shlomikushchi/zipline-livetrader",
+    name='zipline-trader',
+    url="https://github.com/shlomikushchi/zipline-trader",
     version=versioneer.get_version(),
     cmdclass=LazyBuildExtCommandClass(versioneer.get_cmdclass()),
     description='A backtester for financial algorithms.',
